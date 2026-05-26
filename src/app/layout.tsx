c6020ff3fa517006f827e7c8f0eaf00e0496f488
Abhishek Verma <axvrma@gmail.com>
2026-05-26 07:55:36 +0530

    Google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Luminescent - Premium UI Components",
-  description: "A collection of premium, highly-polished, and visually stunning React UI components tailored specifically for modern dark-mode applications.",
+  title: "Luminescent UI | Modern React Components",
+  description: "Premium, highly-polished, and visually stunning React UI components tailored specifically for modern dark-mode applications.",
 };
 
 export default function RootLayout({
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
       </body>
+      <GoogleAnalytics gaId="G-YGFYRJGYFW" />
     </html>
   );
 }
